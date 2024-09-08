ac1219f3a78a30b4642d89436cda1bea59bb42bc
LM22040 <lm22040@ues.edu.sv>
2024-09-08 15:57:52 -0700

    Add files via upload

---
+++ android/support/v4/androidx/core/content/pm/ShortcutXmlParser$$ExternalSyntheticBackport0.java
@@ -0,0 +1,6 @@
+package androidx.core.content.pm;
+
+/* compiled from: D8$$SyntheticClass */
+/* loaded from: classes.dex */
+public final /* synthetic */ class ShortcutXmlParser$$ExternalSyntheticBackport0 {
+}
---
+++ android/support/v4/androidx/core/graphics/ColorUtils$Api26Impl$$ExternalSyntheticBackport0.java
@@ -0,0 +1,9 @@
+package androidx.core.graphics;
+
+/* compiled from: D8$$SyntheticClass */
+/* loaded from: classes2.dex */
+public final /* synthetic */ class ColorUtils$Api26Impl$$ExternalSyntheticBackport0 {
+    public static /* synthetic */ boolean m(Object obj, Object obj2) {
+        return obj == obj2 || (obj != null && obj.equals(obj2));
+    }
+}
